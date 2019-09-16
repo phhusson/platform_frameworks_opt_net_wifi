2941e4c3402d7bc3a9b0cf45610c82bac634f858
Pierre-Hugues Husson <phh@phh.me>
2019-09-16 17:42:37 +0200

    Restore O/O-MR1 behaviour of initing ifaces before supp. Still not perfect, because wifi can be turned on only once
    Change-Id: I7a94b3c4a3fca140a50962e6787af3a7aa2c7d61

--- service/java/com/android/server/wifi/SupplicantStaIfaceHal.java
+++ service/java/com/android/server/wifi/SupplicantStaIfaceHal.java
@@ -741,7 +741,7 @@ public class SupplicantStaIfaceHal {
      * the device.
      * @return true if supported, false otherwise.
      */
-    private boolean isV1_1() {
+    /* package */ boolean isV1_1() {
         return checkHalVersionByInterfaceName(
                 android.hardware.wifi.supplicant.V1_1.ISupplicant.kInterfaceName);
     }
@@ -751,7 +751,7 @@ public class SupplicantStaIfaceHal {
      * the device.
      * @return true if supported, false otherwise.
      */
-    private boolean isV1_2() {
+    /* package */ boolean isV1_2() {
         return checkHalVersionByInterfaceName(
                 android.hardware.wifi.supplicant.V1_2.ISupplicant.kInterfaceName);
     }
